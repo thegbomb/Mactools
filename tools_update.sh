7f04b0a2013239b9dec6a3a0c461fbdd91c27aed
Gareth Alexander <gareth_alexander@hotmail.com>
2020-11-28 16:28:04 +0000

    Create tools_update.sh
    Adding updater script

---
+++ tools_update.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# ------------------------------------------------------------------
+# [gareth alexander] Command line tools updates
+#          Update script to keep command line tools updated
+# ------------------------------------------------------------------
+
+brew update-reset && brew update && brew upgrade
+# brew cask upgrade
+brew upgrade --cask
+npm update -g
+npm install -g npm
+export COMPOSER_PROCESS_TIMEOUT=2000
+composer selfupdate
+composer clear-cache
+composer config -g disable-tls true
+composer config -g secure-http false
+composer global update
+# composer config -g disable-tls false
+# composer config -g secure-http true
+vagrant box prune --keep-active-boxes
