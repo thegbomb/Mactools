b35536cae5397ba4e715f575dd3cc1395c4714b0
Gareth Alexander <gareth_alexander@hotmail.com>
2021-08-23 06:24:50 +0100

    Update tools_update.sh
    Add open ing of tools to help with removal from Dock

--- tools_update.sh
+++ tools_update.sh
@@ -5,13 +5,16 @@
 # ------------------------------------------------------------------
 
 cd ~
+git -C /usr/local/Homebrew/Library/Taps/homebrew/homebrew-cask fetch --unshallow
 brew update-reset && brew update && brew upgrade
 brew outdated
+brew upgrade --cask
 brew list --cask | xargs brew upgrade
 npm update -g
 npm install -g npm
 export COMPOSER_PROCESS_TIMEOUT=2000
 composer selfupdate
+# composer selfupdate --2
 # composer selfupdate --1
 composer clear-cache
 composer config -g disable-tls true
@@ -21,3 +24,20 @@ composer global update
 # composer config -g secure-http true
 vagrant box prune --keep-active-boxes
 cd -
+
+# Open the tools in case they have been remove from the dock
+open -a /Applications/Google\ Chrome.app
+open -a /Applications/Firefox.app
+open -a /Applications/PhpStorm.app
+open -a /Applications/Atom.app/
+open -a /Applications/Visual\ Studio\ Code.app
+open -a /Applications/Meld.app
+open -a /Applications/Sourcetree.app
+# open -a /Applications/Utilities/Terminal.app
+open -a /Applications/Slack.app
+open -a /Applications/zoom.us.app
+open -a /Applications/Spotify.app
+open -a /Applications/Docker.app
+open -a /Applications/Postman.app
+open -a /Applications/Virtualbox.app
+open -a /Applications/Google\ Drive.app
