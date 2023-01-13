7737a9060ba6c77768aeed5207f7bc7646a01e9e
Gareth Alexander <gareth_alexander@hotmail.com>
2023-01-13 00:30:51 +0000

    Update tools_install.sh
    Adding keygen updates and new applications

--- tools_install.sh
+++ tools_install.sh
@@ -12,13 +12,28 @@ xcode-select --install
 mkdir ~/.ssh && touch ~/.ssh/config
 ssh-keygen -t rsa -b 4096 -C "email@here.com"
 cat >> "~/.ssh/config" <<END
+Host *.acquia-sites.com
+   HostKeyAlgorithms=+ssh-rsa
+   PubkeyAcceptedKeyTypes=+ssh-rsa
+
 Host *
   AddKeysToAgent yes
   UseKeychain yes
   IdentityFile ~/.ssh/id_rsa
 END
-ssh-add -K ~/.ssh/id_rsa
+# ssh-add -K ~/.ssh/id_rsa
+ssh-add --apple-use-keychain ~/.ssh/[your-private-key]
 cat ~/.ssh/config
+
+ssh-add -l -E md5
+ssh-keygen -l -E md5 -f ~/.ssh/id_rsa
+ssh-keygen -l -E md5 -f ~/.ssh/id_rsa.pub
+
+# Add keys to:
+#
+# ssh -T git@bitbucket.org
+# ssh -T git@github.com
+# ssh -T git@git.drupal.org
 
 # install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -28,7 +43,7 @@ brew update
 brew doctor
 
 brew install wget curl
-brew install php@7.4 php@8.0 git composer
+brew install php@7.4 php@8.0 php@8.1 git composer
 # brew unlink php && brew link --overwrite --force php@7.4
 brew unlink php && brew link --overwrite --force php@8.0
 brew install mariadb sqlite
@@ -65,6 +80,7 @@ brew install --cask atom phpstorm visual-studio-code
 # Java (if required)
 brew install openjdk
 echo 'export PATH="/usr/local/opt/openjdk/bin:$PATH"' >> ~/.bash_profile
+ln -sfn /usr/local/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 brew install --cask netbeans
 
 # Local development environments
@@ -91,8 +107,10 @@ brew install --cask evernote skitch
 # Others
 brew install --cask adobe-acrobat-reader
 brew install --cask imageoptim
-brew install --cask tableplus
+brew install --cask sequel-ace
+# brew install --cask tableplus
 brew install --cask screaming-frog-seo-spider
+brew install --cask grammarly
 brew install --cask spotify
 
 # Code inspection tools
