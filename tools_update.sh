2efe9264a8a00e6dd7a608604f06f93a43fd9a3d
Gareth Alexander <gareth_alexander@hotmail.com>
2023-01-13 00:22:54 +0000

    Update tools_update.sh
    Adding PHP 8.1 and new Applications

--- tools_update.sh
+++ tools_update.sh
@@ -15,6 +15,8 @@ brew list --cask | xargs brew upgrade --cask
 php -v
 brew unlink php && brew link --overwrite --force php@7.4
 php -v
+brew unlink php && brew link --overwrite --force php@8.1
+php -v
 brew unlink php && brew link --overwrite --force php@8.0
 php -v
 brew services restart php
@@ -24,14 +26,14 @@ npm install -g npm
 export NVM_DIR=$HOME/.nvm;
 source $NVM_DIR/nvm.sh;
 
+nvm install 12
+nvm use 12
+node -v
 nvm install 18
 nvm use 18
 node -v
 nvm install 16
 nvm use 16
-node -v
-nvm install 12
-nvm use 12
 node -v
 
 ulimit -n 10000
@@ -53,6 +55,7 @@ cd -
 # Open the tools in case they have been remove from the dock
 open -a /Applications/Google\ Chrome.app
 open -a /Applications/Firefox.app
+open -a /Applications/Microsoft\ Edge.app
 open -a /Applications/PhpStorm.app
 open -a /Applications/Atom.app/
 # open -a /Applications/Visual\ Studio\ Code.app
@@ -64,10 +67,15 @@ open -a /Applications/zoom.us.app
 open -a /Applications/Spotify.app
 open -a /Applications/Docker.app
 open -a /Applications/Postman.app
-open -a /Applications/TablePlus.app
+open -a /Applications/Sequel\ Ace.app
+# open -a /Applications/TablePlus.app
 # open -a /Applications/Virtualbox.app
 open -a /Applications/Dropbox.app
 open -a /Applications/Google\ Drive.app
 open -a /Applications/Microsoft\ Outlook.app
+open -a /Applications/Microsoft\ Teams.app
+open -a /Applications/Microsoft\ Word.app
+open -a /Applications/Microsoft\ Excel.app
+open -a /Applications/Grammarly\ Editor.app
 # open -a /Applications/Clickup.app
 open -a /Applications/Screaming\ Frog\ SEO\ Spider.app
